cf8092063482a115c77e5c817116dc77ca67703f
Pol Jhon Alariao <apoljhonalariao@gmail.com>
2026-08-10 04:11:54 +1000

    Add NovaGaia Raydium configuration

---
+++ config/token.ts
@@ -0,0 +1,5 @@
+export const NVGAI_TOKEN_ADDRESS = "7gTRGxdbJdyefr3YU8mnrUJxAqjxQfeZ7UjCiYQoWray";
+
+export const NVGAI_RAYDIUM_REFERRAL_URL = "https://raydium.io/launchpad/token/?mint=7gTRGxdbJdyefr3YU8mnrUJxAqjxQfeZ7UjCiYQoWray&fromCreate=true&lreferrer=5GfR4xFdjafe5yKzY3xAeaPFt2z6yzquhVAN1HkvSgbh";
+
+export const NVGAI_EXPLORER_URL = "https://explorer.solana.com/address/7gTRGxdbJdyefr3YU8mnrUJxAqjxQfeZ7UjCiYQoWray";
