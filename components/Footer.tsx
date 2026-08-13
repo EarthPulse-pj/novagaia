e040c9784b6d011a3dee6a07bd927537651d8f93
Pol Jhon Alariao <apoljhonalariao@gmail.com>
2026-08-14 01:55:52 +1000

    Add NovaGaia TikTok link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -207,7 +207,7 @@ export default function Footer() {
               </SocialIcon>
 
 
-              <SocialIcon href="#">
+              <SocialIcon href="https://www.tiktok.com/@novagaia_nvgai">
                 <FaTiktok />
               </SocialIcon>
 
