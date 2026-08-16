6a19fe7d0661af1c83148572d8d4bd926326e268
Pol Jhon Alariao <apoljhonalariao@gmail.com>
2026-08-16 17:29:44 +1000

    Update NovaGaia whitepaper

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -123,7 +123,7 @@ export default function Hero() {
             {/* WHITEPAPER */}
 
             <a
-              href="/NovaGaia-Whitepaper-v1.3.pdf"
+              href="/NovaGaia-Whitepaper-v1.4.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="
